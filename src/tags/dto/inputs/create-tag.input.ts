4ef35a2fc6633301ac56850e6ad004a84551de75
naveeharn <naveeharn@hotmail.com>
2022-11-16 21:49:40 +0700

    add tag module complete

---
+++ src/tags/dto/inputs/create-tag.input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from "@nestjs/graphql"
+import { IsNotEmpty, IsString } from "class-validator"
+
+@InputType()
+export class CreateTagInput {
+
+    @Field()
+    @IsNotEmpty()
+    @IsString()
+    name: string
+
+    // @Field()
+    // @IsNotEmpty()
+    // @IsString()
+    // status: string
+}
---
+++ src/tags/models/tag.ts
@@ -0,0 +1,39 @@
+import { NotAcceptableException } from "@nestjs/common";
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import  { Document } from "mongoose";
+
+@Schema({ timestamps: true })
+@ObjectType()
+export class Tag {
+    @Field(() => ID)
+    _id: string
+
+    @Prop({ type: String, required: [true] })
+    @Field()
+    name: string
+
+    @Prop({ type: String, required: [true], default:'noVerify' })
+    @Field()
+    status: string
+
+    // @Prop({type: Boolean, required: true })
+    @Field()
+    createdAt: Date
+
+    @Field()
+    updatedAt: Date
+}
+
+export const TagSchema = SchemaFactory.createForClass(Tag)
+    .post('save', function (error, doc, next) {
+        if (error.name === 'MongoServerError' && error.code === 11000) {
+            next(new NotAcceptableException('Tag Constraint'));
+        } else {
+            next(error);
+        }
+    });
+
+export type TagDocument = Tag & Document
+
+// export class TagDocument extends Document {}
