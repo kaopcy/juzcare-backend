e7fa3a7a5352165f7f1581c136c13cde77d883ee
naveeharn <naveeharn@hotmail.com>
2022-11-17 16:54:04 +0700

    add avatar service complete

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,16 @@ import { UsersResolver } from './users.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './models/user';
 import { JwtService } from '@nestjs/jwt';
+import { AvatarsService } from 'src/avatars/avatars.service';
+import { Avatar, AvatarSchema } from 'src/avatars/models/avatar';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Avatar.name, schema: AvatarSchema },
+    ]),
   ],
-  providers: [UsersService, UsersResolver, JwtService]
+  providers: [UsersService, UsersResolver, JwtService, AvatarsService]
 })
 export class UsersModule { }
