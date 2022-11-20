62ce7b47b649c2f033d8645372ef376aae1e4022
naveeharn <naveeharn@hotmail.com>
2022-11-20 16:30:53 +0700

    change createTag query and service logic

--- src/tags/tags.resolver.ts
+++ src/tags/tags.resolver.ts
@@ -4,7 +4,9 @@ import { AdminsService } from 'src/admins/admins.service';
 import { Admin } from 'src/admins/models/admin';
 import { CurrentUser } from 'src/auth/current-user.args';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+import { CreateTagInput } from './dto/inputs/create-tag.input';
 import { UpdateTagInput } from './dto/inputs/update-tag.input';
+import { VerifyEnum } from './enum/verify.enum';
 import { Tag } from './models/tag';
 import { TagsService } from './tags.service';
 
@@ -14,6 +16,16 @@ export class TagsResolver {
         private readonly tagsService: TagsService,
         private readonly adminsService: AdminsService,
     ) {}
+
+    @Mutation(() => Tag)
+    @UseGuards(GqlAuthGuard)
+    async createTag(@CurrentUser() admin: Admin,@Args('createTagData') createTagData: CreateTagInput) : Promise<Tag> {
+        const tag = await this.tagsService.createTag(createTagData)
+        if (await this.adminsService.isAdmin(admin._id)) {
+            return await this.updateStatusTag(admin, {_id: tag._id, status: "VERIFIED" as VerifyEnum})
+        }
+        return tag
+    }
 
     @Query(() => [Tag])
     async getVerifiedTags(): Promise<Tag[]> {
