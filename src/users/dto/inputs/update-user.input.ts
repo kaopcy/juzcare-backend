c862a413ca6aed8d792b4946fa97af786e8d6e7e
naveeharn <naveeharn@hotmail.com>
2022-11-22 08:18:31 +0700

    edit validation

--- src/users/dto/inputs/update-user.input.ts
+++ src/users/dto/inputs/update-user.input.ts
@@ -4,56 +4,56 @@ import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator"
 
 @InputType()
 export class UpdateUserInput {
-    @Field(() => ID)
-    @IsNotEmpty({ message: 'ไอดีว่างอยู่' })
-    @IsString()
-    _id: string
+    // @Field(() => ID)
+    // @IsNotEmpty({ message: 'ไอดีว่างอยู่' })
+    // @IsString()
+    // _id: string
 
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'อีเมล์ว่างอยู่' })
+    // @IsNotEmpty({ message: 'อีเมล์ว่างอยู่' })
     @IsString()
     email?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'รูปแบบไอดีว่างอยู่' })
+    // @IsNotEmpty({ message: 'รูปแบบไอดีว่างอยู่' })
     @IsString()
     emailType?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'ชื่อผู้ใช้ว่างอยู่' })
+    // @IsNotEmpty({ message: 'ชื่อผู้ใช้ว่างอยู่' })
     @IsString()
     username?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'ชื่อจริงว่างอยู่' })
+    // @IsNotEmpty({ message: 'ชื่อจริงว่างอยู่' })
     @IsString()
     firstName?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'นามสกุลว่างอยู่' })
+    // @IsNotEmpty({ message: 'นามสกุลว่างอยู่' })
     @IsString()
     lastName?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'เบอร์โทรว่างอยู่' })
+    // @IsNotEmpty({ message: 'เบอร์โทรว่างอยู่' })
     @IsString()
     phone?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'ตำแหน่งงานว่างอยู่' })
+    // @IsNotEmpty({ message: 'ตำแหน่งงานว่างอยู่' })
     @IsString()
     role?: string
     
     @Field({ nullable: true })
     @IsOptional()
-    @IsNotEmpty({ message: 'สถานะไอดีว่างอยู่' })
+    // @IsNotEmpty({ message: 'สถานะไอดีว่างอยู่' })
     @IsBoolean()
     isBannned?: boolean
 }
